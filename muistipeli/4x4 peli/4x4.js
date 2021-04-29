1e03d5df15b1f36270c02cf4dc0ad0d4424d167f
jeppuu <jennamariakoskelo@gmail.com>
2021-04-29 14:00:30 +0300

    4x4 peli toimii chromessa

--- muistipeli/4x4 peli/4x4.js
+++
@@ -1,7 +0,0 @@
-const kortit = document.querySelectorAll('.kortti');
-
-function kaannaKortti() {
-  this.class.toggle('flip');
-}
-
-kortit.forEach(kortti => kortti.addEventListener('click',kaannaKortti));
